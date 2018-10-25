8f9c69e1c80d10c765bcd3bcb6949a042562cc5c
ascordero <anjocordero@gmail.com>
2018-10-25 00:36:19 -0700

    add number one songs and artist

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ import requests
 page = requests.get('https://www.billboard.com/charts/hot-100')
 soup = BeautifulSoup(page.content, 'html.parser')
 songs = soup.find_all(class_='chart-list-item__title-text')
+songs.insert(0, soup.find(class_='chart-number-one__title'))
 artists = soup.find_all(class_='chart-list-item__artist')
+artists.insert(0, soup.find(class_='chart-number-one__artist'))
 print([song.get_text(strip=True) for song in songs])
 print([artist.get_text(strip=True) for artist in artists])
