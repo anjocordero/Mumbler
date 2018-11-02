77a475908f6268d4a353785d07e87a402099b1f3
ascordero <anjocordero@gmail.com>
2018-11-01 17:17:53 -0700

    adds SongList to file, currently times out when searching for lyrics

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Mumbler
-Procedurally generates pop lyrics based on Top 100 songs
+Procedurally generates pop lyrics based on Billboard Top 100 songs
+
+## Dependencies
+- googlesearch
+  + install through
+> pip install google
+- Beautiful Soup 4
+
+Known Issues:
+- Currently times out when searching for lyrics, possibly sending too many requests
--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from googlesearch import search
 from song import Song
 import requests
+import time
 
 page = requests.get('https://www.billboard.com/charts/hot-100')
 soup = BeautifulSoup(page.content, 'html.parser')
@@ -27,11 +28,35 @@ else:
 #for song in SongList:
 #    print(song.title + ", " + song.artist)
 
-for song, i in zip(SongList, range(5)):
+for song, i in zip(SongList, range(100)):
+
+    if i == 40 or i == 80:
+        time.sleep(10)
+    else:  
+        time.sleep(2)
+
     url = list(search(song.title + ' ' + song.artist + ' azlyrics.com', stop=1))[0]
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     text = soup.find(class_='col-xs-12 col-lg-8 text-center')
-    lyrics = text.find_all('div')[6]
-    song.lyrics = list(lyrics.stripped_strings)
-    print(song.lyrics)
+    found = False
+
+    if text:
+        lyrics = text.find_all('div')[6]
+        song.lyrics = list(lyrics.stripped_strings)
+
+    else:
+        song.lyrics = []
+
+try:
+    f = open('./SongList.txt', 'x')
+    for song in SongList:
+        f.write(song.title)
+        f.write(song.artist)
+        for lyric in song.lyrics:
+            f.write(lyric)
+        f.write("\n")
+
+except FileNotFoundError:
+    print("File already exists.")
+    exit()
