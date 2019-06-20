999260d78fb467092040ecfa5837ce9df96d69d6
ascordero <anjocordero@gmail.com>
2019-06-20 16:04:26 -0700

    functional lyric generation, added generate.py to create one sentence at a time

--- createMarkov.py
+++ createMarkov.py
@@ -1,16 +1,37 @@
 import os
+from os import path
+import warnings
 
 from markovchain import JsonStorage
 from markovchain.text import MarkovText, ReplyMode
 
 from parser import chartSwitcher, lyricDirectory
 
+markovDir = "Markov"
+rootDir = chartSwitcher()
+
+# Suppress reg. exp. warning that shows when using markovchain library
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+def create_directory():
+    """Creates directory for .json markovchain databases"""
+
+    # Create directory for all lyrics
+    try:
+        os.mkdir(markovDir)
+    except FileExistsError:
+        pass
+
 
 def main():
 
-    rootDir = chartSwitcher()
     markov = MarkovText()
 
+    # Remove .json if it already exists
+    if path.exists('%s.json' % (rootDir)):
+        os.remove('%s.json' % (rootDir))
+
+    # Iterate through folders in selected genre
     for dirName, subdirList, fileList in os.walk(lyricDirectory + "/" + rootDir):
 
         # Ignore hidden directories like .git
@@ -32,7 +53,8 @@ def main():
 
             with open(dirName + "/" + fname) as fp:
                 for line in fp:
-                    markov.data(line, part=False)
+                    if not line.startswith("["):
+                        markov.data(line, part=False)
 
             print("\tAdded song: " + fname)
 
@@ -40,14 +62,14 @@ def main():
         # Unsure if necessary, was included in markovchain documentation
         markov.data('', part=False)
 
-        print(markov())
-        print(markov(max_length=16, reply_to='sentence start',
-                     reply_mode=ReplyMode.END))
+        # print statements not functional as is
+        # print(markov())
+        # print(markov(max_length=16, reply_to='sentence start',
+        #              reply_mode=ReplyMode.END))
 
-        markov.save('%s.json' % (rootDir))
-
-        markov = MarkovText.from_file('markov.json')
+        markov.save('%s/%s.json' % (markovDir, rootDir))
 
 
 if __name__ == '__main__':
+    create_directory()
     main()
---
+++ generate.py
@@ -0,0 +1,32 @@
+import os
+import warnings
+from parser import chartSwitcher
+
+import createMarkov
+from markovchain import JsonStorage
+from markovchain.text import MarkovText, ReplyMode
+
+# Suppress reg. exp. warning that shows when using markovchain library
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+markovDir = "Markov"
+markovScript = "markov.py"
+
+def main():
+
+    selectedChart = chartSwitcher()
+
+    try:
+        markov = MarkovText.from_file('%s/%s.json' % (markovDir, selectedChart))
+    except FileNotFoundError:
+        print("%s.json file not found. Using markov.py to create one first!" %
+              selectedChart)
+        createMarkov.create_directory()
+        createMarkov.main()
+        markov = MarkovText.from_file('%s/%s.json' % (markovDir, selectedChart))
+
+    print(markov())
+
+
+if __name__ == '__main__':
+    main()
